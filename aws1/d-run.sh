7e164d162268538bf0cd5100d0c45dc245a64489
drypot <drypot@gmail.com>
2021-02-22 14:05:01 +0000

    update.

--- aws1/d-run.sh
+++ aws1/d-run.sh
@@ -3,8 +3,8 @@ args=(
   --name mariadb
   --user 1001:1001
   -p 3306:3306
-  --mount type=bind,source=/data/mysql/conf.d,target=/etc/mysql/conf.d
-  --mount type=bind,source=/data/mysql/data,target=/var/lib/mysql
+  --mount type=bind,source=/data/mysql/mysql-conf/aws1/conf.d,target=/etc/mysql/conf.d
+  --mount type=bind,source=/data/mysql/mysql,target=/var/lib/mysql
   --mount type=bind,source=/data/mysql/backup,target=/backup
   -e MYSQL_ROOT_PASSWORD=
   -e MYSQL_ALLOW_EMPTY_PASSWORD=yes
